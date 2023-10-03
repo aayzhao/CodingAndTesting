1df7e869b7e57fbc8e1878518b8ab0536f2d110c
Aaron Zhao <aaron.y.zhao1@gmail.com>
2023-10-03 11:39:51 -0400

    Leetcode practice 10/3/2023

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
 
+
+
 //        System.out.println(solution.winnerOfGame("AA"));
 //        System.out.println(solution.winnerOfGame("ABBBBBBBAAA"));
 //        System.out.println(solution.winnerOfGame("AAAABBBB"));
